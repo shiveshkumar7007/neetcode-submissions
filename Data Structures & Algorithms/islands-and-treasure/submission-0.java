f08a248a2359521e8161cc2ee88d1773c31ca973
shiveshkumar7007 <165477955+shiveshkumar7007@users.noreply.github.com>
2026-06-09 12:56:22 +0530

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    int k = Integer.MAX_VALUE;
+    public void islandsAndTreasure(int[][] grid) {
+        if(grid.length == 0){
+            return;
+        }
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 0){
+                    dfs(grid, i, j, 0);
+                }
+                
+            }
+        }
+                
+    }
+
+    public void dfs(int[][] grid, int r, int c, int dist){
+        if(r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == -1 || grid[r][c] < dist){
+            return;
+        }
+        grid[r][c] = dist;
+        dfs(grid, r, c+1, dist+1);
+        dfs(grid, r, c-1, dist+1);
+        dfs(grid, r+1, c, dist+1);
+        dfs(grid, r-1, c, dist+1);
+    }
+}
