7119086a4276fc7727179976655f9605b93d67c1
shiveshkumar7007 <165477955+shiveshkumar7007@users.noreply.github.com>
2026-06-22 11:52:34 +0530

    Add: partition-to-k-equal-sum-subsets - submission-4

---
+++ Data Structures & Algorithms/partition-to-k-equal-sum-subsets/submission-4.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int len = nums.length;
+        if(len < k){
+            return false;
+        }
+        int sum = 0;
+        for(int num: nums){
+            sum += num;
+        }
+        if(sum % k != 0){
+            return false;
+        }
+        int target = sum / k;
+        int[] arr = new int[k];
+        return dfs(0, k, nums, target, arr);        
+    }
+
+    public boolean dfs(int ind, int k, int[] nums, int target, int[] arr){
+        if(ind == nums.length){
+            for(int i = 0; i < k; i++){
+                if(arr[i] != target){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        int num = nums[ind];
+        for(int i = 0; i < k; i++){
+            if(num + arr[i] > target){
+                continue;
+            }
+            arr[i] += num;
+
+            if(dfs(ind+1, k, nums, target, arr)){
+                return true;
+            }
+
+            arr[i] -= num;
+            // very important step 
+            // it is called pruning
+            if(arr[i] == 0){ // very imp
+                break;
+            }
+        }
+        return false;
+    }
+}
