414f1d1f361208a06ac42ef4a8e2f1b353d42a4c
shiveshkumar7007 <165477955+shiveshkumar7007@users.noreply.github.com>
2026-06-29 22:51:10 +0530

    Add: find-lucky-integer-in-an-array - submission-0

---
+++ Data Structures & Algorithms/find-lucky-integer-in-an-array/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int ans = -1;
+        for (int num : map.keySet()) {
+            if (num == map.get(num)) {
+                ans = Math.max(ans, num);
+            }
+        }
+        return ans;
+    }
+}
