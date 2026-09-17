4a37fe92a486bba7c5043265ade9836384ea37fc
shiveshkumar7007 <165477955+shiveshkumar7007@users.noreply.github.com>
2026-09-17 21:59:12 +0530

    Add: copy-linked-list-with-random-pointer - submission-3

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-3.java
@@ -0,0 +1,31 @@
+// O(N) O(N)
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null){
+            return head;
+        }
+
+        Map<Node, Node> map = new HashMap<>();
+
+        Node cur = head;
+        while(cur != null){
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+
+        cur = head;
+        while(cur != null){
+            Node copy = map.get(cur);
+
+            copy.next = map.get(cur.next);
+            copy.random = map.get(cur.random);
+
+            cur = cur.next;
+        }
+
+        return map.get(head);
+
+
+    }
+}
