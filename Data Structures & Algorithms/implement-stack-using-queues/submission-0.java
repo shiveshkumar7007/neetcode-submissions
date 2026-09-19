4e64456a46d33b2a2c25fe907082ff3fc4a5ccc4
shiveshkumar7007 <165477955+shiveshkumar7007@users.noreply.github.com>
2026-09-19 10:44:05 +0530

    Add: implement-stack-using-queues - submission-0

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-0.java
@@ -0,0 +1,36 @@
+class MyStack {
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        // Put new element in q2
+        q2.add(x);
+
+        // Move all old elements behind x
+        while (!q1.isEmpty()) {
+            q2.add(q1.remove());
+        }
+
+        // Swap q1 and q2
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+    
+    public int pop() {
+        return q1.remove();
+    }
+    
+    public int top() {
+        return q1.peek();
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
